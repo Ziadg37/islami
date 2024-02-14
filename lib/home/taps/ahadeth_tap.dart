6a3361b79f14af505ef4bfcd1d9f207271e290fd
ziad gamal <ziadg3776@gmail.com>
2024-02-14 21:10:18 +0200

    hadeth details screen finished

--- lib/home/taps/ahadeth_tap.dart
+++ lib/home/taps/ahadeth_tap.dart
@@ -1,13 +1,91 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:islami/hadeth_details.dart';
 
-class AhadethTap extends StatelessWidget {
-  const AhadethTap({super.key});
+import '../../modle/hadeth_modle.dart';
+
+class AhadethTap extends StatefulWidget {
+  AhadethTap({super.key});
+
+  @override
+  State<AhadethTap> createState() => _AhadethTapState();
+}
+
+class _AhadethTapState extends State<AhadethTap> {
+  List<HadethModel> hadethData = [];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepPurple,
+    if (hadethData.isEmpty) {
+      loadhadethFile();
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset(
+          "assets/images/ahadeth.png",
+          width: double.infinity,
+        ),
+        Divider(
+          thickness: 3,
+          color: Color(0xffB7935F),
+        ),
+        Text(
+          "Ahadeth",
+          style:
+              GoogleFonts.elMessiri(fontSize: 20, fontWeight: FontWeight.w400),
+        ),
+        Divider(
+          thickness: 3,
+          color: Color(0xffB7935F),
+        ),
+        Expanded(
+          child: ListView.separated(
+            separatorBuilder: (context, index) =>
+                Divider(color: Color(0xffB7935F),endIndent: 40, indent: 40,thickness:1),
+            itemBuilder: (context, index) {
+
+
+
+              return InkWell(
+                onTap: (){
+                  
+                  Navigator.pushNamed(context, HadethDetails.routeName,arguments: hadethData[index]);
+
+                },
+
+                child:Text(
+                hadethData[index].title,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.elMessiri(
+                    fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+
+              );
+            },
+            itemCount: hadethData.length,
+          ),
+        )
+      ],
     );
+  }
+
+  loadhadethFile() {
+    rootBundle.loadString("assets/files/ahadeth.txt").then((hadethFile) {
+      List<String> ahadeth = hadethFile.split("#");
+      for (int i = 0; i < ahadeth.length; i++) {
+        String hadeth = ahadeth[i];
+        List<String> hadethLine = hadeth.trim().split("\n");
+        String title = hadethLine[0];
+        hadethLine.removeAt(0);
+        List<String> hadethContent = hadethLine;
+        print(title);
+        hadethData.add(HadethModel(title: title, content: hadethContent));
+      }
+      setState(() {});
+    });
   }
 }
--- lib/sura_details.dart
+++ lib/sura_details.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:islami/sura_model.dart';
+import 'package:islami/modle/sura_model.dart';
 
 class SuraDetailsScreen extends StatefulWidget {
   static const String routeName = "sura";
